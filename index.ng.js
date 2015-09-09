a386687b4aca1cb12b7b2c16e3a288a16621f35d
mdctenga <mdctenga@gmail.com>
2015-09-08 19:05:36 -1000

    Display Mongo data through Angular

--- index.ng.js
+++ index.ng.js
@@ -1,8 +1,27 @@
+Locations = new Mongo.Collection("locations");
+
 if (Meteor.isClient) {
   //declare module
   angular.module('jog-it-off',['angular-meteor']);
 
-  // angular.module('jog-it-off').controller('PartiesListCtrl', ['$scope', '$meteor', function($scope, $meteor) {
-  //   $scope.parties = $meteor.collection(Parties);
-  // }]);
+  angular.module('jog-it-off').controller('LocationsListCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+    $scope.locations = $meteor.collection(Locations);
+  }]);
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Locations.find().count() === 0) {
+      var locations = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+      for (var i = 0; i < locations.length; i++)
+        Locations.insert(locations[i]);
+    }
+  });
 }
