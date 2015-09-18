ceca4001a028cbe53df45c5a8954551c3392286c
Jenn L <jenn.lazariuk@gmail.com>
2015-09-17 18:07:55 -1000

    pushing up templates for team members

--- client/lib/games/views/game_starts.js
+++ client/lib/games/views/game_starts.js
@@ -1,35 +1,15 @@
-var countdown = function(end, elements, callback) {
-  var _second = 1000,
-      _minute = _second * 60,
-      _hour = _minute * 60,
-      _day = _hour * 24,
+Template.game_starts.rendered = function(){
+  countdown('10/11/2013 06:30:00 PM', ['days', 'hours', 'minutes', 'seconds'], function(){
+      console.log('Finished');
+    });
+};
 
-      end = new Date(end),
-      timer,
+  var intervalTimer = setInterval(function(){
+    if(currentCount === 0){
+      clearInterval(intervalTimer);
+      return Router.go('/set_point');
+    }
 
-      calculate = function() {
-
-        var now = new Date(),
-            remaining = end.getTime() - now.getTime(),
-            data;
-
-        if(isNaN(end)){
-          console.log('Invalid date/time');
-          return;
-        }
-
-        if(remaining <= 0) {
-          // route to next page
-        } else {
-          if(!timer) {
-            timer = setInterval(calculate, _second);
-          }
-        }
-
-        //
-
-        console.log('second');
-      };
-
-  calculate();
-}
+    currentCount = self.currentSeconds.get() - 1;
+    self.currentSeconds.set(currentCount);
+  }, 1000);
