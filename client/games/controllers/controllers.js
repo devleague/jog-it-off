b03aa660eb10b6b4bd7c1e7868ee1f38143c8ac7
mdctenga <mdctenga@gmail.com>
2015-09-19 11:30:32 -1000

    Move timer controller functionality to controller file

--- client/games/controllers/controllers.js
+++ client/games/controllers/controllers.js
@@ -7,3 +7,30 @@
   }]);
 
 
+angular.module('jog-it-off')
+  .controller('timerController', TimerController);
+
+  function TimerController($scope, $meteor, $interval) {
+    $scope.timer = "5:00";
+
+    var fiveMinutes = 60 * 5;
+    startTimer(fiveMinutes);
+
+    function startTimer(duration) {
+      var timer = duration;
+      $interval(function () {
+        var minutes = parseInt(timer / 60, 10);
+        var seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        textContent = minutes + ":" + seconds;
+
+        $scope.timer = textContent;
+
+        if (--timer < 0) {
+          timer = duration;
+        }
+      }, 1000);
+    }
+  }
