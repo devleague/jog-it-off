d90674ef148d6fafd74fb96c7258f570a2fe0dfe
Raymond Mariano <rcm@hawaii.edu>
2015-09-23 01:54:22 -1000

    able to make plotTimer display count down and stop at 0

--- client/games/controllers/setPointController.ng.js
+++ client/games/controllers/setPointController.ng.js
@@ -3,16 +3,17 @@
     .controller('setPoint', function ($scope, $interval, $state, $meteor, $rootScope, JogService, $stateParams) {
 
       $scope.timer = Meteor.user().profile.game.plotTimer;
-      // var num = 5;
+      var num = $scope.timer;
+      console.log(num);
 
-      // $interval(function() {
-      //     console.log($scope.countNum);
-      //     num--;
-      //     $scope.countNum = num;
-      //     if($scope.countNum <= 0) {
-      //       $state.go('set_point');
-      //     }
-      //   }, 1000, 5);
+      $interval(function() {
+          console.log($scope.timer);
+          num--;
+          $scope.timer = num;
+          if($scope.timer <= 0) {
+            $state.go('set_point');
+          }
+        }, 1000, num);
 
 
        });
