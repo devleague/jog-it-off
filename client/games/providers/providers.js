d9ab466d361c11b68838e4ffca39ab2b141b2e7c
Raymond Mariano <rcm@hawaii.edu>
2015-09-23 01:20:05 -1000

    make sure player cannot join a game more than once

--- client/games/providers/providers.js
+++ client/games/providers/providers.js
@@ -34,11 +34,24 @@
 
       this.joinGame = function ($scope, $meteor) {
         var client = Meteor.user();
+        var clientID = Meteor.userId();
+        var gameID = this.game._id;
         var gameObj = GameCollection.findOne({_id: gameID});
+        var players = gameObj.players;
 
-        if(gameObj.players.indexOf(client) < 0) {
+        //find out if client is existing in players array
+        var missing = false;
+        for(var i=0; i < players.length; i++) {
+          if(players[i]._id !== clientID) {
+            missing = true;
+          }
+        }
+
+        //if missing, add client to players array
+        if(missing) {
           GameCollection.update({_id: this.game._id}, {$push: {players: client}});
         }
+        // Meteor.users.update({_id: clientID}, {$set: {game: gameObj}});
 
       };
 
