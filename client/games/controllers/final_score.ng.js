4c6f106b5a1347950b1115ddefb63ee903b5e69e
Jenn L <jenn.lazariuk@gmail.com>
2015-10-03 21:13:14 -1000

    adding results controller

--- client/games/controllers/final_score.ng.js
+++ client/games/controllers/final_score.ng.js
@@ -9,15 +9,4 @@ angular
         console.log('return me to main page');
         $state.go('main');
       }
-
-      // $scope.winner = winner;
-
-      // function winner () {
-      //   for(var i = 0; i < gameObj.players.length; i++) {
-      //     //sort out per
-      //   }
-      // }
-
-
-
 });
