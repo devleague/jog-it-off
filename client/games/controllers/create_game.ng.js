4d38d1f6e325e5335a783c6d0e0a13918a100380
Jenn L <jenn.lazariuk@gmail.com>
2015-10-08 19:30:34 -1000

    halfway through styles

--- client/games/controllers/create_game.ng.js
+++ client/games/controllers/create_game.ng.js
@@ -3,4 +3,5 @@
     .controller('createGameController', function ($scope, $state, $meteor, $rootScope, JogService, $stateParams) {
 
       $scope.addGameObject = JogService.addGameObject;
+      $rootScope.wink = true;
   });
