9c0853e4a531abc3a3f63975e206051ae9947485
Raymond Mariano <rcm@hawaii.edu>
2015-10-20 03:01:19 -1000

    remove the remove GameCollection

--- client/games/controllers/join.ng.js
+++ client/games/controllers/join.ng.js
@@ -10,13 +10,6 @@
       var now = new Date().getTime();
       var day = 86400000;
 
-      //remove all
-      GameCollection
-        .find({})
-        .forEach(function(x) {
-          GameCollection.remove({_id: x._id});
-        });
-
       //remove games that have already ended
       GameCollection
         .find({gameTimer: {$lt:1}})
